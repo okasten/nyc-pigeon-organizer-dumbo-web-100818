9b5fc038d52aac48dee34195eddd08d1356d35ee
Olivia Kasten <odkasten@gmail.com>
2018-09-27 13:17:04 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  index = 0
+  organized = {}
+  data.each do |attribute, specifier|
+    specifier.each do |kind, name|
+      #binding.pry
+      if organized.keys.include?(name[index])
+        organized[name[index]][attribute] = kind.to_s
+        binding.pry
+      else 
+        organized[name[index]][attribute] = kind.to_s
+      end 
+      index += 1
+    end
+    index = 0
+  end 
+  organized
 end
